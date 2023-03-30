4fc060d7331ea6a196c2ddad081e823c69b18502
mohsenbh67 <behroozi.mohsen1@gmail.com>
2023-03-30 15:02:14 +0430

    Route change

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -41,9 +41,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        // RateLimiter::for('api', function (Request $request) {
+        //     return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        // });
 
         RateLimiter::for('user-register-limiter', function (Request $request) {
             return Limit::perMinute(5)->by($request->ip());
